30d2a77e5c6307c2f917bc9dcde47d61351261af
Riley Siebel <riley.siebel@gmail.com>
2012-04-28 21:07:39 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Investor::Application.config.secret_token = '60db0d350e095cd301b6b3789f8f0d00af8872a52803c79f6321c75397dddf4c6c39f1a09e907a6777d02cb2220ded42f8851e5fd091cffc5df0230845cbd9d7'
