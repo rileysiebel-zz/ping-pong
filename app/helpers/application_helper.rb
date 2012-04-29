ebeb133e6a371ce0391e3339e9dba717e6c4da13
Riley Siebel <riley.siebel@gmail.com>
2012-04-29 12:43:50 -0700

    Refactoring Static Pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Return the correct title for a particular page
+  def title
+    @base_title = "Investor"
+    if @title.nil?
+      @base_title
+    else
+      "#{@base_title} | #{@title}"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Investor::Application.routes.draw do
   get "pages/home"
-
   get "pages/contact"
-  
   get "pages/about"
 
   # The priority is based upon order of creation:
